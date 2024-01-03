0ea370bd6d001baa7314f7a8109b11102f166c50
JaeYeopHan <ljyhanll@gmail.com>
2024-01-03 14:19:36 +0900

    feat: add health route

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,11 +5,18 @@ import Fastify from "fastify";
 import { initialize } from "../database/seed.js";
 import { isProduction } from "../env/node-env.js";
 
+const port = Number(process.env.PORT ?? 4000);
+
 const fastify = Fastify({
   logger: true
 });
+
 fastify.get("/", async () => {
   return { hello: "world" };
+});
+
+fastify.get("/health", async (_, reply) => {
+  return reply.statusCode = 200;
 });
 
 const start = async () => {
@@ -17,7 +24,7 @@ const start = async () => {
     if (!isProduction) {
       await initialize();
     }
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port });
     
   } catch (err) {
     fastify.log.error(err);
